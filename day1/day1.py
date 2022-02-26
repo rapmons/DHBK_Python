dc0970ce13ca99b1f636ddb3ea4be4e079a7be44
BINH <77979118+rapmons@users.noreply.github.com>
2022-02-26 15:47:42 +0700

    update day1

---
+++ day1/day1.py
@@ -0,0 +1,44 @@
+print("Hello World!\n Hello world!\nHello World!")
+#Hello World!
+#Hello World!
+#Hello World!
+print("Hello"+""+"Angela")
+#Hello Angela
+print("Day1-String Manipulation")
+print("String Concatenation is done with the"+" sign.")
+print('e.g.Print("hello"+"world")')
+print("New lines can be created with a backslash and n.")
+
+#Input() Func
+#input() will get user input in console
+# then print() will print the word "Hello" and the user input
+print("Hello"+input("what is your name?")+"!")
+# What is your name? Angela
+# Hello Angela !
+
+#input Exercise
+print(len(input("what is your name?")))
+
+# variables
+name= input("what is your name?")
+print(name)
+length= len(name)
+print(length)
+
+# Exercise variables
+print("a="+input("a:"))
+print("b="+input("b:"))
+
+#Variale Naming
+# ##########################
+#1.create a greeting for your program.
+print("wellcom to the band name generator")
+#2.ask the user foe the city that they grew up in
+city=input("which city did you grow up in?\n")
+print(city)
+#3.ask the user for the name of a pet
+pet= input("what is the name of a pet?\n")
+#4.combine the name of their city and pet and show them their band name.
+print("your band name could be"+city+" "+pet)
+#5.Make sure the input cursor shows on a new line,see the example at:
+
